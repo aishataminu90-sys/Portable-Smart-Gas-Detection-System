1921a5c8798265f688713c08c581caa1a2a43175
aishat <aishatAminu90@gmail.com>
2026-04-15 15:19:58 +0100

    Added gas detection with threshholds

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-print("Portable Smart Gas Detection System Starting...")
+import time
+
+print("?? Portable Smart Gas Detection System Starting...")
+
+THRESHOLD_HIGH = 300
+THRESHOLD_MEDIUM = 150
 
 while True:
-    gas_level = int(input("Enter gas level: "))
+    try:
+        gas_level = int(input("Enter gas level: "))
 
-    if gas_level > 300:
-        print("?? Gas Leak Detected!")
-    elif gas_level > 150:
-        print("?? Warning: Gas Level Rising")
-    else:
-        print("? Safe Levels")
+        print(f"Gas Level: {gas_level}")
+
+        if gas_level > THRESHOLD_HIGH:
+            print("?? DANGER: Gas Leak Detected!")
+        elif gas_level > THRESHOLD_MEDIUM:
+            print("?? WARNING: Gas Level Rising")
+        else:
+            print("?? Safe Levels")
+
+        print("-" * 30)
+        time.sleep(1)
+
+    except ValueError:
+        print("? Please enter a valid number")
